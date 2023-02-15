29bc841b989e2dcb6395299c8da787fb83cbd226
niuniu <bluefuxianwu@gmail.com>
2023-02-15 18:11:45 +0800

    解决echarts配置项在删除换行后导致无法解析的问题

--- src/ts/markdown/chartRender.ts
+++ src/ts/markdown/chartRender.ts
@@ -16,8 +16,9 @@ export const chartRender = (element: (HTMLElement | Document) = document, cdn = 
                     e.parentElement.classList.contains("vditor-ir__marker--pre")) {
                     return;
                 }
-
-                const text = chartRenderAdapter.getCode(e).trim();
+                
+                // 解决getCode(e)方式获取的text无换行的问题
+                const text = e.innerHTML; // chartRenderAdapter.getCode(e).trim();
                 if (!text) {
                     return;
                 }
@@ -28,10 +29,17 @@ export const chartRender = (element: (HTMLElement | Document) = document, cdn = 
                     
                     // const option = JSON.parse(text);
                     // echarts.init(e, theme === "dark" ? "dark" : undefined).setOption(option);
+                    // 替换默认逻辑为以下代码，此行删除前后的空白字符
                     const txtStr = text.replace(/^\s+|\s+$/g,''); 
+                    const txtBody = 'var option; \n' + (txtStr.startsWith('{') ? 'option = ' : '') + txtStr + '\nreturn option;';
                     
-                    var optionFun = new Function('myChart',  'setTimeout', 'setInterval', 'var option; \n' + (txtStr.startsWith('{') ? 'option = ' : '') + txtStr + '\nreturn option;');
+                    // 输入文本的函数定义，如果有其他参数请加入，https://fuyiyi.imdo.co 
+                    var optionFun = new Function('myChart',  'setTimeout', 'setInterval', txtBody);
+                    
+                    // 原chartRender中的实例化后直接设置Option，现在拆开
                     var myChart = echarts.init(e, theme === "dark" ? "dark" : undefined);
+                    
+                    // 通过原 JS 形式配置项 获取最终的option，https://fuyiyi.imdo.co 
                     const option = optionFun(myChart, setTimeout, setInterval);
                     myChart.setOption(option);
 
